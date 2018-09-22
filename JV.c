e8a7a8057303215b7f4e69fcd3a2cfa84f9dc98d
GustavoM7 <gustavosmarquesf@gmail.com>
2018-09-22 17:01:56 -0300

    Removendo bugs, incrementando funções.

--- JV.c
+++ JV.c
@@ -4,27 +4,166 @@
 
 
 //Função para checagem de fim de jogo:
-int check(char matriz[3][3]){
-	int i = 0, j = 0, cont = 0;
-
-	while(matriz[i][j] != ' ' && i < 3){
-		j = 0;
-		while(matriz[i][j] != ' ' && j < 3){
-			j++;
-			cont++;			
-		}
-		i++;
-	}
-
-	if (cont == 9){
-		return 0;
-	} else {
-		return 1;
-	}
-}
+int check(char **matriz);
 
 //Função para imprimir tabela no terminal:
-void imprime(char matriz[3][3]){
+void imprime(char **matriz);
+
+//Função para pausar o jogo;
+void pausar();
+
+int main (){
+	int linha, coluna, i, j;
+
+	printf("\n---Bem vindo ao Jogo da Velha em C---\n\n");	
+	printf("Encerre o jogo com comando Ctrl + C\n");
+	pausar();
+
+	char **matriz = (char**) malloc(3 * sizeof(char*));
+
+	for (i = 0; i < 3; i++){
+		matriz[i] = (char*) malloc(3 * sizeof(char));
+
+		for (j = 0; j < 3; j++){
+			matriz[i][j] = ' ';
+		}
+
+	}
+
+	imprime(matriz);
+
+	while(check(matriz) == 1){
+		printf("Jogador X: Entre linha e coluna\nLinha: ");
+		scanf(" %d", &linha);
+
+		while(linha > 3 || linha < 1){
+			printf("Entrada inválida!\n");
+			printf("\nLinha(1 <--> 3): ");
+			scanf(" %d", &linha);
+
+		}
+
+		printf("Coluna: ");
+		scanf(" %d", &coluna);
+
+		while(coluna > 3 || coluna < 1){
+			printf("Entrada inválida!\n");
+			printf("\nColuna(1 <--> 3): ");
+			scanf(" %d", &coluna);
+
+		}
+
+		matriz[linha-1][coluna-1] = 'X';
+		imprime(matriz);
+
+		printf("Jogador O: Entre linha e coluna\nLinha: ");
+		scanf(" %d", &linha);
+
+		while(linha > 3 || linha < 1){
+			printf("Entrada inválida!\n");
+			printf("\nLinha(1 <--> 3): ");
+			scanf(" %d", &linha);
+
+		}
+
+		printf("Coluna: ");
+		scanf(" %d", &coluna);
+
+		while(coluna > 3 || coluna < 1){
+			printf("Entrada inválida!\n");
+			printf("\nColuna(1 <--> 3): ");
+			scanf(" %d", &coluna);
+
+		}
+
+		matriz[linha-1][coluna-1] = 'O';
+		imprime(matriz);
+	}
+	
+	printf("\nJogo ecerredo, Parabéns ao vencedor!\n");
+	free(matriz);
+	return 0;
+}
+
+void pausar(){
+	char pause[41];
+	printf("Pressione Enter para continuar...");
+	scanf("%[^\n]", pause);
+}
+
+int check(char **matriz){
+	int i, j, k;
+
+	k = 0;
+	//Procurando vitoria de X na diagonal 1:
+	for (i = 0; i < 3; i++){
+		if (matriz[i][i] == 'X'){
+			k++;
+		}
+		
+		if (k == 3){
+			//Jogador X vencedor:
+			return 2;
+		}
+	}
+
+	k = 0;
+	//Procurando vitoria de X na diagonal 2:
+	for (i = 0; i < 3; i++){
+		if (matriz[i][3 - i] == 'X'){
+			k++;
+		}
+		
+		if (k == 3){
+			//Jogador X vencedor:
+			return 2;
+		}
+	}
+
+	k = 0;
+	//Procurando vitoria de O na diagonal 1:
+	for (i = 0; i < 3; i++){
+		if (matriz[i][i] == 'O'){
+			k++;
+		}
+		
+		if (k == 3){
+			//Jogador O vencedor:
+			return 3;
+		}
+	}
+
+	k = 0;
+	//Procurando vitoria de O na diagonal 2:
+	for (i = 0; i < 3; i++){
+		if (matriz[i][3 - i] == 'O'){
+			k++;
+		}
+		
+		if (k == 3){
+			//Jogador O vencedor:
+			return 3;
+		}
+	}
+
+
+	//Procurando espaços vazios:
+	for (i = 0; i < 3; i++){
+		for (j = 0; j < 3; j++){
+			if (matriz[i][j] == ' '){
+				//Jogo em progresso:
+				return  1;
+			}
+		
+		}
+
+	}
+
+	//Velha:
+	return 0;
+}
+
+void imprime(char **matriz){
 	printf("\e[H\e[2J");
 	printf("\n\n	 %c | %c | %c \n", matriz[0][0], matriz[0][1], matriz[0][2]);
 	printf("	--- --- ---\n");
@@ -33,26 +172,3 @@ void imprime(char matriz[3][3]){
 	printf("	 %c | %c | %c \n\n\n", matriz[2][0], matriz[2][1], matriz[2][2]);
 }
 
-//printf("\e[H\e[2J");
-int main (){
-	int linha, coluna;
-	int pause;
-	printf("\n---Bem vindo ao Jogo da Velha em C---\n\n");	
-	printf("Entre qualquer numero para continuar...\n");
-	printf("Encerre o jogo com comando Ctrl + C\n");
-	scanf("%d", &pause);
-	char matriz[3][3] = {{' ', ' ', ' '}, {' ', ' ', ' '}, {' ', ' ', ' '}};
-	system("PAUSE");
-	imprime(matriz);
-	while(check(matriz)){
-		printf("Jogador X: Entre linha e coluna\nLinha: ");
-		scanf("%d", &linha);
-		printf("Coluna: ");
-		scanf("%d", &coluna);
-		matriz[linha][coluna] = 'X';
-		imprime(matriz);
-	}
-		
-	system("PAUSE");
-	return 0;
-}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Jogo-da-Velha
 Jogo da Velha em C
+
+-- Versão atual --
+* Presente:
+- Detecção do jogador vencedor ou velha (sem influência no código).
+- Detecção de vitórias nas diagonais.
+- Função de pause.
+- Uso de ponteiros
+
+* Em progresso:
+- Detecção de vitorias nas linhas e colunas.
+- Cores no terminal.
+- Tratamento de erros de entrada.
+- Opção de jogo contra máquina.
